629b5d7a487dbce4e7505c940835f8e5bc676c2a
Dylan <s89673827@gmail.com>
2021-03-25 15:51:36 +0800

    Initial Commit

---
+++ Poker_hw2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Poker_hw2
+//
+//  Created by 朱冠儒 on 2021/3/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Poker_hw2/Poker_hw2App.swift
@@ -0,0 +1,17 @@
+//
+//  Poker_hw2App.swift
+//  Poker_hw2
+//
+//  Created by 朱冠儒 on 2021/3/25.
+//
+
+import SwiftUI
+
+@main
+struct Poker_hw2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
